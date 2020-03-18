ef3b48c06f6370e91ec4f88fff58f106ab88de88
Vince Weaver <vincent.weaver@maine.edu>
2020-03-18 15:53:45 -0400

    uname: whitespace changes

--- libsysinfo-0.2.2/all/uname.c
+++ libsysinfo-0.2.2/all/uname.c
@@ -3,27 +3,28 @@
 #include <string.h>      /* strncpy */
 #include <sys/utsname.h> /* uname   */
 
-int uname_get_os_info(struct os_info_type *os_info) {  
-    
-    struct utsname buf;
+int uname_get_os_info(struct os_info_type *os_info) {
 
-    uname(&buf);
+	struct utsname buf;
 
-    strncpy(os_info->os_name,buf.sysname,SYSINFO_OS_NAME_SIZE);
-    strncpy(os_info->os_version,buf.release,SYSINFO_OS_VERSION_SIZE);
-    strncpy(os_info->os_revision,buf.version,SYSINFO_OS_REVISION_SIZE);
-/*   os_info->host_name=strdup(buf.nodename); */
-    return 0;
+	uname(&buf);
+
+	strncpy(os_info->os_name,buf.sysname,SYSINFO_OS_NAME_SIZE);
+	strncpy(os_info->os_version,buf.release,SYSINFO_OS_VERSION_SIZE);
+	strncpy(os_info->os_revision,buf.version,SYSINFO_OS_REVISION_SIZE);
+/*	os_info->host_name=strdup(buf.nodename); */
+	return 0;
 }
 
 char *uname_get_host_name(char *hostname,char *domain) {
-    
-    struct utsname buf;
-   
-    uname(&buf);
-   
-    strncpy(hostname,buf.nodename,SYSINFO_HOSTNAME_SIZE);
-/*    strncpy(domain,buf.domainname,64);*/
-    strncpy(domain,"Unknown",SYSINFO_DOMAIN_SIZE);  /* Can't get there from here */
-    return hostname;
+
+	struct utsname buf;
+
+	uname(&buf);
+
+	strncpy(hostname,buf.nodename,SYSINFO_HOSTNAME_SIZE);
+/*	strncpy(domain,buf.domainname,64);*/
+	/* Can't get there from here */
+	strncpy(domain,"Unknown",SYSINFO_DOMAIN_SIZE);
+	return hostname;
 }
