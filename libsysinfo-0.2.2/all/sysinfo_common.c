ddcfb7a93a0a622ad376430af709ceffafca2f42
Vince Weaver <vincent.weaver@maine.edu>
2020-03-18 15:51:54 -0400

    sysinfo_common: whitespace changes

--- libsysinfo-0.2.2/all/sysinfo_common.c
+++ libsysinfo-0.2.2/all/sysinfo_common.c
@@ -5,36 +5,37 @@
 #include <stdio.h>
 
 struct our_state {
-    int pretty_printing;
-    int custom_cpuinfo;
-    char cpuinfo_file[BUFSIZ];
+	int pretty_printing;
+	int custom_cpuinfo;
+	char cpuinfo_file[BUFSIZ];
 };
 
 struct our_state internal_state={1,0,"/proc/cpuinfo"};
 
 char *get_sysinfo_version(char *version) {
-    snprintf(version,BUFSIZ,"%i.%i.%i",VERSION_MAJOR,VERSION_MINOR,VERSION_SUBMINOR);
-    return version;
+	snprintf(version,BUFSIZ,"%i.%i.%i",
+			VERSION_MAJOR,VERSION_MINOR,VERSION_SUBMINOR);
+	return version;
 }
 
 int get_numeric_sysinfo_version(void) {
-    return (VERSION_MAJOR*10000)+(VERSION_MINOR*100)+VERSION_SUBMINOR;  
+	return (VERSION_MAJOR*10000)+(VERSION_MINOR*100)+VERSION_SUBMINOR;
 }
 
 int set_cpuinfo_file(char *filename) {
-    internal_state.custom_cpuinfo=1;
-    strncpy(internal_state.cpuinfo_file,filename,BUFSIZ);
-    return 0;
+	internal_state.custom_cpuinfo=1;
+	strncpy(internal_state.cpuinfo_file,filename,BUFSIZ);
+	return 0;
 }
 
 char *get_cpuinfo_file(void) {
-    return internal_state.cpuinfo_file;  
+	return internal_state.cpuinfo_file;
 }
 
 void set_pretty_printing(int value) {
-   internal_state.pretty_printing=value;
+	internal_state.pretty_printing=value;
 }
 
 int get_pretty_printing(void) {
-   return internal_state.pretty_printing;
+	return internal_state.pretty_printing;
 }
