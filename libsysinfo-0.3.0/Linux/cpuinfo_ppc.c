752da17f147c3151bba52424cb610055261ab92b
Vince Weaver <vince@deater.net>
2020-05-09 12:56:52 -0400

    libsysinfo: ppc: clean up whitespace

--- libsysinfo-0.3.0/Linux/cpuinfo_ppc.c
+++ libsysinfo-0.3.0/Linux/cpuinfo_ppc.c
@@ -11,149 +11,154 @@
 
 int get_cpu_info(struct cpu_info_type *cpu_info) {
 
-    FILE *fff;
-    char temp_string[BUFSIZ],temp[BUFSIZ];
-    char vendor_string[BUFSIZ],model_string[BUFSIZ];
-    int cpu_count=0,processor_count=0,bogomips_count=0;
-    float megahertz=0.0,bogomips=0.0;
-   
-    vendor_string[0]=model_string[0]=0;
- 
-       /* We get all of our info here from /proc/cpuinfo */
-    if ((fff=fopen(get_cpuinfo_file(),"r") )!=NULL) {
-       
-       while ( (fgets(temp_string,BUFSIZ,fff)!=NULL) ) {
-	
-	  if ( !(strncmp(temp_string,"cpu",3))) {
-	     strncpy(model_string,parse_line(temp_string),BUFSIZ);
-	     clip_lf(model_string,BUFSIZ);
-	  }
-	  
-	  if (!(strncmp(temp_string,"clock",5))) {
-	     megahertz=atof(parse_line(temp_string));  
-	  }
+	FILE *fff;
+	char temp_string[BUFSIZ],temp[BUFSIZ];
+	char vendor_string[BUFSIZ],model_string[BUFSIZ];
+	int cpu_count=0,processor_count=0,bogomips_count=0;
+	float megahertz=0.0,bogomips=0.0;
 
-	  if ( !(strncmp(temp_string,"processor",9))) {
-	     processor_count++;
-	  }
-	  
-	     /* Ugh why must people play with capitalization */
-	  if ( !(strncmp(temp_string,"bogomips",8)) ||
-	       !(strncmp(temp_string,"BogoMips",8)) ||
-	       !(strncmp(temp_string,"BogoMIPS",8))) {
-	     bogomips+=atof(parse_line(temp_string));
-	     bogomips_count++;  /* Cheating way to detect number of intel CPUs */
-	  }
-       }
-    }
-   
-       /* on cell, there is no bogomips count! */
-    if (bogomips_count==0) {
-       cpu_count=processor_count;
-    }
-    else {
-	cpu_count=bogomips_count;
-    }
-   
-    strncpy(cpu_info->chip_vendor,"PPC",4);
-    strncpy(cpu_info->chip_type,model_string,SYSINFO_CHIP_TYPE_SIZE);
+	vendor_string[0]=model_string[0]=0;
 
-    if (strstr(model_string,"POWER3")!=NULL) {
-       strncpy(cpu_info->chip_type,"POWER3",7);
-    }
-    if (strstr(model_string,"POWER4")!=NULL) {
-       strncpy(cpu_info->chip_type,"POWER4",7);
-    }
-    if (strstr(model_string,"POWER5")!=NULL) {
-       strncpy(cpu_info->chip_type,"POWER5",7);
-    }
-    if (strstr(model_string,"POWER6")!=NULL) {
-       strncpy(cpu_info->chip_type,"POWER6",7);
-    }
-   
-    if (strstr(model_string,"POWER7")!=NULL) {
-       strncpy(cpu_info->chip_type,"POWER7",7);
-    }
+	/* We get all of our info here from /proc/cpuinfo */
+	if ((fff=fopen(get_cpuinfo_file(),"r") )!=NULL) {
 
-    if (strstr(model_string,"POWER8")!=NULL) {
-       strncpy(cpu_info->chip_type,"POWER8",7);
-    }
-       /* I Have an iBook now ;) */
-    if (strstr(model_string,"745/755")!=NULL) {
-       strncpy(cpu_info->chip_type,"G3",3);
-    }
-       
-       /* But that's not the only iBook around! ;) */
-    if (strstr(model_string,"750CX")!=NULL) {
-       strncpy(cpu_info->chip_type,"G3",3);	    
-    }
-   
-    if (strstr(model_string,"750FX")!=NULL) {
-       strncpy(cpu_info->chip_type,"G3",3);	    
-    }
-   
-    if (strstr(model_string,"7400")!=NULL) {
-       strncpy(cpu_info->chip_type,"7400",5);
-    }
- 
-       /* There should be a way to figure out */
-       /* G4/G3 more efficiently than this    */
-    if (strstr(model_string,"7410,")!=NULL) {
-       strncpy(cpu_info->chip_type,"G4",3);
-    }
-   
-    if (strstr(model_string,"7455")!=NULL) {
-       strncpy(cpu_info->chip_type,"G4",3);
-    }
-   
-       /* I have a powerbook now too ;) */
-    if (strstr(model_string,"7457")!=NULL) {
-       strncpy(cpu_info->chip_type,"G4",3);
-    }
+		while ( (fgets(temp_string,BUFSIZ,fff)!=NULL) ) {
 
-    if (strstr(model_string,"Cell")!=NULL) {
-       strncpy(cpu_info->chip_type,"Cell",5);
-    }
-  	
-    if (!strncmp(model_string,"PowerPC",7)) {
-       sscanf(model_string,"%*s %s",temp);
-       strncpy(cpu_info->chip_type,temp,SYSINFO_CHIP_TYPE_SIZE);
-    }
-  
-    cpu_info->num_cpus=cpu_count;
-    cpu_info->megahertz=megahertz;
-    cpu_info->bogomips=bogomips;
+			if ( !(strncmp(temp_string,"cpu",3))) {
+				strncpy(model_string,parse_line(temp_string),BUFSIZ);
+				clip_lf(model_string,BUFSIZ);
+			}
 
-    return 0;
-   
+			if (!(strncmp(temp_string,"clock",5))) {
+				megahertz=atof(parse_line(temp_string));
+			}
+
+			if ( !(strncmp(temp_string,"processor",9))) {
+				processor_count++;
+			}
+
+			/* Ugh why must people play with capitalization */
+			if ( !(strncmp(temp_string,"bogomips",8)) ||
+				!(strncmp(temp_string,"BogoMips",8)) ||
+				!(strncmp(temp_string,"BogoMIPS",8))) {
+
+				/* Cheating way to detect number of CPUs */
+				bogomips+=atof(parse_line(temp_string));
+				bogomips_count++;
+			}
+		}
+	}
+
+	/* on cell, there is no bogomips count! */
+	if (bogomips_count==0) {
+		cpu_count=processor_count;
+	}
+	else {
+		cpu_count=bogomips_count;
+	}
+
+	strncpy(cpu_info->chip_vendor,"PPC",4);
+	strncpy(cpu_info->chip_type,model_string,SYSINFO_CHIP_TYPE_SIZE);
+
+	if (strstr(model_string,"POWER3")!=NULL) {
+		strncpy(cpu_info->chip_type,"POWER3",7);
+	}
+
+	if (strstr(model_string,"POWER4")!=NULL) {
+		strncpy(cpu_info->chip_type,"POWER4",7);
+	}
+
+	if (strstr(model_string,"POWER5")!=NULL) {
+		strncpy(cpu_info->chip_type,"POWER5",7);
+	}
+
+	if (strstr(model_string,"POWER6")!=NULL) {
+		strncpy(cpu_info->chip_type,"POWER6",7);
+	}
+
+	if (strstr(model_string,"POWER7")!=NULL) {
+		strncpy(cpu_info->chip_type,"POWER7",7);
+	}
+
+	if (strstr(model_string,"POWER8")!=NULL) {
+		strncpy(cpu_info->chip_type,"POWER8",7);
+	}
+
+	/* I Have an iBook now ;) */
+	if (strstr(model_string,"745/755")!=NULL) {
+		strncpy(cpu_info->chip_type,"G3",3);
+	}
+
+	/* But that's not the only iBook around! ;) */
+	if (strstr(model_string,"750CX")!=NULL) {
+		strncpy(cpu_info->chip_type,"G3",3);
+	}
+
+	if (strstr(model_string,"750FX")!=NULL) {
+		strncpy(cpu_info->chip_type,"G3",3);
+	}
+
+	if (strstr(model_string,"7400")!=NULL) {
+		strncpy(cpu_info->chip_type,"7400",5);
+	}
+
+	/* There should be a way to figure out */
+	/* G4/G3 more efficiently than this    */
+	if (strstr(model_string,"7410,")!=NULL) {
+		strncpy(cpu_info->chip_type,"G4",3);
+	}
+
+	if (strstr(model_string,"7455")!=NULL) {
+		strncpy(cpu_info->chip_type,"G4",3);
+	}
+
+	/* I have a powerbook now too ;) */
+	if (strstr(model_string,"7457")!=NULL) {
+		strncpy(cpu_info->chip_type,"G4",3);
+	}
+
+	if (strstr(model_string,"Cell")!=NULL) {
+		strncpy(cpu_info->chip_type,"Cell",5);
+	}
+
+	if (!strncmp(model_string,"PowerPC",7)) {
+		sscanf(model_string,"%*s %s",temp);
+		strncpy(cpu_info->chip_type,temp,SYSINFO_CHIP_TYPE_SIZE);
+	}
+
+	cpu_info->num_cpus=cpu_count;
+	cpu_info->megahertz=megahertz;
+	cpu_info->bogomips=bogomips;
+
+	return 0;
 }
 
 int get_hardware(char *hardware_string) {
-    
-    char temp_string[BUFSIZ];
-    FILE *fff;
-   
-    if ((fff=fopen(get_cpuinfo_file(),"r") )!=NULL) {
-       
-       while ( (fgets(temp_string,BUFSIZ,fff)!=NULL) ) {
-	  	  
-	  if (!(strncmp(temp_string,"machine",7))) {
-             strncpy(hardware_string,parse_line(temp_string),
-		     SYSINFO_HARDWARE_STRING_SIZE);
-	  }
 
-       }
-    }
-    return 1;
+	char temp_string[BUFSIZ];
+	FILE *fff;
+
+	if ((fff=fopen(get_cpuinfo_file(),"r") )!=NULL) {
+
+		while ( (fgets(temp_string,BUFSIZ,fff)!=NULL) ) {
+
+			if (!(strncmp(temp_string,"machine",7))) {
+				strncpy(hardware_string,parse_line(temp_string),
+					SYSINFO_HARDWARE_STRING_SIZE);
+			}
+
+		}
+	}
+	return 1;
 }
 
-    /* Some architectures might have better ways of detecting RAM size */
+	/* Some architectures might have better ways of detecting RAM size */
 long long get_arch_specific_mem_size(void) {
-       /* We have no special way of detecting RAM */
-   
-       /* sysinfo is the only way that works on cell? */
-       /* can we detect that somehow and force it? */
-   
-       /* For mac the meminfo way is definitely better */     
-       return MEM_USE_MEMINFO;
+	/* We have no special way of detecting RAM */
+
+	/* sysinfo is the only way that works on cell? */
+	/* can we detect that somehow and force it? */
+
+	/* For mac the meminfo way is definitely better */
+	return MEM_USE_MEMINFO;
 }
